5a1bb7441eaa19c94b5feead7450a21443ec032d
Eric Yu <ericy27@gmail.com>
2026-05-18 14:07:37 -0700

    Update ast.h

--- src/ast.h
+++ src/ast.h
@@ -68,5 +68,46 @@ struct DeclVarStmtNode : StmtNode {
     std::unique_ptr<ExprNode> init;
 };
 
+struct AssignStmtNode : StmtNode {
+    std::unique_ptr<ExprNode> target;   // VarExpr or IndexExpr
+    std::unique_ptr<ExprNode> value;
+};
+
+struct IfStmtNode : StmtNode {
+    std::unique_ptr<ExprNode> condition;
+    std::vector<std::unique_ptr<StmtNode>> then_body;
+    std::vector<std::unique_ptr<StmtNode>> else_body;
+};
+
+struct ForStmtNode : StmtNode {
+    std::string var;
+    std::unique_ptr<ExprNode> start;
+    std::unique_ptr<ExprNode> end;
+    std::vector<std::unique_ptr<StmtNode>> body;
+};
+
+struct ReturnStmtNode : StmtNode {
+    std::unique_ptr<ExprNode> value;    // nullptr for void return
+};
+
+struct ExprStmtNode : StmtNode {
+    std::unique_ptr<ExprNode> expr;
+};
+
+// top level
+struct Param {
+    std::string name;
+    Type type;
+};
+
+struct KernelDefNode {
+    std::string name;
+    std::vector<Param> params;
+    std::vector<std::unique_ptr<StmtNode>> body;
+};
+
+struct ProgramNode {
+    std::vector<std::unique_ptr<KernelDefNode>> kernels;
+};
 
 
